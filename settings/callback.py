2232894016257a0b654f9e3ced038cc236d48001
Al-Evst <al.evst96@gmail.com>
2025-03-29 15:53:23 +0100

    Обновил форму отказа пользователя

--- settings/callback.py
+++ settings/callback.py
@@ -3,7 +3,7 @@ from aiogram.fsm.context import FSMContext
 import datetime
 from .state import CreateOrder
 from .keyboard import (choose_action_keyboard, choose_money_keyboard, choose_color_keyboard,
-                       choose_flower_keyboard, create_calendar, create_time_control_keyboard)
+                       choose_flower_keyboard, create_calendar, create_time_control_keyboard, user_agreement_keyboard)
 from .db_helper import get_flowers_id, get_byid_flower, get_another_ids_flowers
 from numpy import frombuffer, uint8
 from .message_helper import edit_image_keyboard, create_consultation
@@ -21,7 +21,8 @@ async def user_agreement_callback(callback: types.CallbackQuery, state: FSMConte
         await state.set_state(CreateOrder.choose_action)
         await callback.message.answer('К какому событию готовимся?', reply_markup= await choose_action_keyboard())
     if data == 'no':
-        await callback.message.answer('Вы не дали согласие')
+        user_agreement = types.FSInputFile("user_agreement.pdf")
+        await callback.message.answer_document(user_agreement, caption="Для работы с Телеграм-ботом необходимо ваше согласие", reply_markup=user_agreement_keyboard())
 
 
 async def choose_action_callback(callback: types.CallbackQuery, state: FSMContext):
